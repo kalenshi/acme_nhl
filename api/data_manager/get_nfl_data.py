d50981045160ecd14a24fb6d69ade9ab9f5c4602
Kalenshi Katebe <kalenshi@gmail.com>
2021-02-01 14:10:43 -0400

    Worked on parsing data from the expernal Api

--- api/data_manager/get_nfl_data.py
+++ api/data_manager/get_nfl_data.py
@@ -1,7 +1,11 @@
 import requests
+import json
+import datetime
+import pandas as pd
+from decimal import Decimal
 
 score_board_url = "https://delivery.chalk247.com/" \
-                  "scoreboard/NFL/2020-01-12/" \
+                  "scoreboard/NFL/2020-01-13/" \
                   "2020-01-19.json?api_key" \
                   "=74db8efa2a6db279393b433d97c2bc843f8e32b0"
 rankings_url = "https://delivery.chalk247.com/team_rankings" \
@@ -10,10 +14,37 @@ rankings_url = "https://delivery.chalk247.com/team_rankings" \
 
 def api_call(date_range=None):
     try:
-        score_data = requests.get(score_board_url).json()['results']
-        rankings_data = requests.get(rankings_url).json()['results']['data']  # This is an array
-        ['event_id', 'event_data', 'away_team_id', 'away_nick_name', 'away_city']
-        ['rank', 'adjusted_points', '']
-        return score_data
+        scores_data = requests.get(score_board_url).json()['results']
+        rankings_data = requests.get(rankings_url).json()['results']['data']
+        scoreboard_results = {event_key: scores_data[score]['data'][event_key]
+                              for score in scores_data.keys() if len(scores_data[score]) > 0
+                              for event_key in scores_data[score]['data'].keys()}
+
+        for sb in scoreboard_results:
+            event_date = datetime.datetime.strptime(scoreboard_results[sb]['event_date'], "%Y-%m-%d %H:%M")
+            scoreboard_results[sb]['event_time'] = event_date.strftime('%H:%M')
+            scoreboard_results[sb]['event_date'] = event_date.strftime('%d-%m-%Y')
+
+        api_keys_subset = [
+            "event_id", "event_date", "away_team_id", "away_nick_name",
+            "away_city", "home_team_id", "home_city", "home_nick_name",
+            "event_time"
+        ]
+        frame = pd.DataFrame(scoreboard_results).transpose()[api_keys_subset]
+        new_score_data_json = json.loads(frame.to_json(orient='records'))
+        df = pd.DataFrame(rankings_data)[['team_id', 'rank', 'adjusted_points']]
+        new_ranks_json = json.loads(df.to_json(orient='records'))
+        for event in new_score_data_json:
+            for ranking in new_ranks_json:
+                if event['away_team_id'] == ranking["team_id"]:
+                    event['away_rank'] = ranking['rank']
+                    event['away_rank_points'] = '{0:.2f}'.format(Decimal(ranking['adjusted_points']))
+                    break
+            for ranking in new_ranks_json:
+                if event['home_team_id'] == ranking["team_id"]:
+                    event['home_rank'] = ranking['rank']
+                    event['home_rank_points'] = '{0:.2f}'.format(Decimal(ranking['adjusted_points']))
+                    break
+        return new_score_data_json
     except Exception as e:
-        print("Error")
+        return {"Error: ", e}
